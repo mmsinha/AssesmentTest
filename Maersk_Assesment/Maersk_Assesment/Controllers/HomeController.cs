1773993c066aafb85d0fbf45ab61f62616f63b7e
mmsinha <43377785+mmsinha@users.noreply.github.com>
2020-07-13 21:01:14 +0530

    Add files via upload

---
+++ Maersk_Assesment/Maersk_Assesment/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Maersk_Assesment.Models;
+
+namespace Maersk_Assesment.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            BusinessRules rules = new BusinessRules();
+
+
+            return View(rules);
+        }
+        [HttpPost]
+        public ActionResult Index(BusinessRules rule)
+        {
+            if (rule.businessRule == DropDownList.ForBook)
+            {
+                //create a duplicate packing slip for the royalty department.
+                //generate a commission payment to the agent. 
+            }
+            else if (rule.businessRule == DropDownList.ForanUpgradeMembership)
+            {
+                // apply the upgrade
+                //e-mail the owner and inform them of the activation/upgrade. 
+
+            }
+            else if (rule.businessRule == DropDownList.ForMemberShip)
+            {
+                //activate that membership
+                //e-mail the owner and inform them of the activation/upgrade. 
+
+            }
+            else if (rule.businessRule == DropDownList.ForPhysicalProduct)
+            {
+                //generate a packing slip for shipping. 
+                //generate a commission payment to the agent. 
+            }
+            else if (rule.businessRule == DropDownList.ForVideoLearning)
+            {
+                //add a free "First Aid" video to the packing slip (the result of a court
+            }
+
+            BusinessRules rules = new BusinessRules();
+            return View(rules);
+        }
+
+    }
+}
---
+++ Maersk_Assesment/Maersk_Assesment/Models/BusinessRules.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Maersk_Assesment.Models
+{
+    public enum DropDownList
+    {
+        ForPhysicalProduct=0,
+        ForBook=1,
+        ForMemberShip=2,
+        ForanUpgradeMembership=3,
+        ForVideoLearning=4
+    }
+    public class BusinessRules
+    {
+        public DropDownList businessRule { get; set; }  
+        public IEnumerable<SelectListItem> dropdownRuleList { get; set; }
+        public BusinessRules()  
+        {
+            dropdownRuleList = Enum.GetNames(typeof(DropDownList)).Select(name => new SelectListItem()  
+            {  
+                Text = name,  
+                Value = name  
+            });  
+        }
+    }
+}
